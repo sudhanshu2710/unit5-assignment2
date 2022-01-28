25ba2aec24eb5787ab07879f2ea2ccc047da9a8d
sudhanshu2710 <sudhanshu.singh2710@gmail.com>
2022-01-28 13:06:30 +0530

    hello

---
+++ script.js
@@ -0,0 +1,20 @@
+const teacher = function (address, degree) {
+  this.address = address;
+  this.degree = degree;
+};
+
+const person = function (name, subject, address, degree) {
+  const newProperties = teacher.bind(this);
+  newProperties(address, degree);
+  this.name = name;
+  this.subject = subject;
+};
+
+const p1 = new person("aryan", "maths", "varanasi", "U.G.");
+const p2 = new person("sam", "physics", "delhi", "P.G.");
+const p3 = new person("robert", "bio", "banglore", "U.G.");
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p1.address);
