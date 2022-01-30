d484446d4a066957d4e337ca6da2266e426aa26f
sudhanshu2710 <sudhanshu.singh2710@gmail.com>
2022-01-30 17:06:51 +0530

    hi

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const person = function (name, subject, address, degree) {
 
 const p1 = new person("aryan", "maths", "varanasi", "U.G.");
 const p2 = new person("sam", "physics", "delhi", "P.G.");
-const p3 = new person("robert", "bio", "banglore", "U.G.");
+const p3 = new person("robert", "bio", "banglore", "U.G");
 
 console.log(p1);
 console.log(p2);
